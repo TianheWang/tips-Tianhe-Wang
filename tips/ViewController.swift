0161837536275ecc57e0f58bb15f356b76039210
Tianhe Wang <wang_tianhe@163.com>
2015-08-29 15:15:38 -0700

    Basic function
    Add some basic function

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -5,14 +5,28 @@
 //  Created by Tianhe Wang on 7/27/15.
 //  Copyright (c) 2015 Tianhe Wang. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
+    var tipPercentage: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        // load tip percentage from defaults
+        tipPercentage = defaults.valueForKey("tipPercentage") as? Double
+        tipControl.selectedSegmentIndex = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        // save tipPercentage
+        defaults.setValue(tipPercentage, forKey: "tipPercentage")
+    }
+    
+ 
 
 }
 
